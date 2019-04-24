a4ea04e03089d311e1d2c54686a84787d028b162
kbbushman <k.bushman@yahoo.com>
2019-04-24 14:08:25 -0700

    Initial commit, adds server boilerplate

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+// ---------------------------------------- MIDDLEWARE ---------------------------------------- //
+
+// Parse URL Encoded Form Data
+app.use(bodyParser.urlencoded({extended: true}));
+// Parse JSON Data
+app.use(bodyParser.json());
+
+// Serve Public Directory
+app.use(express.static(__dirname + '/public'));
+
+// ------------------------------------------ ROUTES ------------------------------------------ //
+
+// GET Root Route
+app.get('/', (req, res) => {
+  res.send('<h1>Auth App</h1>');
+});
+
+// --------------------------------------- START SERVER --------------------------------------- //
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
